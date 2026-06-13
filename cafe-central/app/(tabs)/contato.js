6fdc473eba8b0e31f5904802aeb5990c32b2bb72
guizn31 <guilhermesalves1601@gmail.com>
2026-06-12 23:22:49 -0300

    pagina de contato e sobre

--- cafe-central/app/(tabs)/contato.js
+++ cafe-central/app/(tabs)/contato.js
@@ -1,108 +1,217 @@
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { Link } from 'expo-router';
+import {
+    View, // Para agrupar elementos (= div)
+    Text, // Para exibir textos (= p, h1...)
+    TouchableOpacity, // Para botões clicáveis (= button)
+    ScrollView, // Para a área principal com scroll,
+    StyleSheet // Para aplicar estilo na página
+   } from 'react-native'; // Importa os componentes View e Text
+   import {Link} from 'expo-router';
+   import { useState } from 'react';
+import { TextInput } from 'react-native-web';
+   
+  export default function Contato() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [mensagem, setMensagem] = useState('');
 
-export default function contato() {
-  return (
-    <ScrollView>
-      <View style={styles.topo}>
+    const [mensagemSistema, setMensagemSistema] = useState('');
+    const [tipoMensagem, setTipoMensagem] = useState('');  // 'sucesso' ou 'erro'
 
-      <Link href='/'>
-        <Text style={styles.logo1}>Café</Text>
-        <Text style={styles.logo2}>Central</Text>
-      </Link>
+    function validarFormulario() {
+      if (nome === '') {
+        setMensagemSistema('Digite seu nome.');
+        setTipoMensagem('erro');
+        return false;
+      }
+      if (/\d/.test(nome)) {
+        setMensagemSistema('O nome não pode conter números.');
+        setTipoMensagem('erro');
+        return false;
+      }
 
+      if (email === '') {
+        setMensagemSistema('Digite seu email.');
+        setTipoMensagem('erro');
+        return false;
+      }
 
-        <View style={styles.menu}>
+      if (email.includes('@') === false || email.includes('.com') === false) {
+        setMensagemSistema('Digite um email válido.');
+        setTipoMensagem('erro');
+        return false;
+      }
+
+      if (mensagem === '') {
+        setMensagemSistema('Digite sua mensagem.');
+        setTipoMensagem('erro');
+        return false;
+      }
+
+      if (mensagem.length < 10) {
+        setMensagemSistema('A mensagem deve conter pelo menos 10 caracteres.');
+        setTipoMensagem('erro');
+        return false;
+      }
+
+      setMensagemSistema('Mensagem enviada com sucesso!');
+      setTipoMensagem('sucesso');
+      
+      setNome('');
+      setEmail('');
+      setMensagem('');
+    };
+
+   return (
+      <ScrollView>
+          { /*=========== TOPO (HEADER) =============*/}
+          { /*=========== Área de cabeçalho com logo e menu =============*/}
+          <View style={styles.topo}>
+  
+          { /* Logo do sistema */}
           <Link href='/'>
-            <Text style={[styles.menuItem,styles.ativo]}> Inicio </Text>
+            <Text style={styles.logoP1}>Tech</Text>
+            <Text style={styles.logoP2}>Educa</Text>
           </Link>
-        
-          <Link href='/sobre'>
-            <Text style={styles.menuItem}> Sobre </Text>
-          </Link>
+  
+            { /* Menu de Navegação */}
+            <View style={styles.menu}>
+              <Link href='/'>
+                <Text style={styles.menuItem}> Início </Text>
+              </Link>
+              <Link href='/sobre'>
+                <Text style={styles.menuItem}> Sobre </Text>
+              </Link>
+              <Link href='/contato'>
+                <Text style={[styles.menuItem, styles.ativo]}> Contato </Text>
+              </Link>
+            </View>
+          </View>
 
-          <Link href='/contato'>
-            <Text style={styles.menuItem}> Contato </Text>
-          </Link>
+          { /*=========== CONTEÚDO DA PÁGINA =============*/}
+          <View style = {styles.container}>
+            {/* Card branco que envolve o form */}
+            <View style={styles.cardContato}>
+              <Text style={styles.tituloDestaque}>
+                Entre em contato
+              </Text>
+              <Text style={styles.subtitulo}>
+                Envie sua mensagem para nossa equipe.
+              </Text>
 
-        </View>
-      </View>
+              <TextInput style={styles.TextInput}
+               placeholder= 'Digite seu nome'
+               value={nome} // exibe o estado da variavel
+               onChangeText={setNome} // Atualiza o estado da variavel
+               ></TextInput>
 
+              <TextInput style={styles.TextInput}
+               placeholder= 'Digite seu email'
+               keyboardType='email-address'
+               value={email}
+               onChangeText={setEmail}
+              ></TextInput>
 
-      { /* Conteudo da pagina*/}
+              <TextInput style={styles.TextInput}
+               placeholder= 'Digite sua mensagem'
+               multiline={true} 
+               value={mensagem} 
+               onChangeText={setMensagem}
+              ></TextInput>
 
+              <TouchableOpacity style={styles.btnEnviar} 
+                onPress={validarFormulario}>
+                <Text style={styles.textobtn}>
+                  Enviar Mensagem
+                </Text>
+              </TouchableOpacity>
 
-      { /*=========== rodape =============*/}
+              <text style={tipoMensagem=== 'erro' ? styles.mensagemErro : styles.mensagemSucesso}>
+                {mensagemSistema}
+              </text>
+            </View>
+          </View>
+          
 
-
-      <View style={styles.rodape}>
-        <Text style={styles.textoRodape}>®2026 Café Central. Todos os Direitos Reservados</Text>
-        <Link href='/contato'>
-          <Text style={styles.linkRodape}>Entre em contato</Text>
-        </Link>
-      </View>
-
-     
-
-    </ScrollView>
-  );
-}
-
-
-const styles = StyleSheet.create(
-  {
-    topo:{
-      backgroundColor: '#976739',
-      padding: 20,
-      alignItems: 'center',
-      gap: 10
-    },
-
-    logo1:{
-      color: '#ffffff',
-      fontSize: 24,
-      fontWeight:'bold'
-    },
-
-    logo2:{
-      color: '#e6b061',
-      fontSize: 24,
-      fontWeight:'bold'
-    },
-
-    menu:{
-      marginTop: 10,
-      alignItems: 'center',
-      gap: 20,
-    },
-
-    menuItem:{
-      color: '#ffffff',
-      fontSize: 16,
-    },
-    
-
-
-    rodape:{
-      backgroundColor: '#976739',
-      textAlign: 'center',
-      marginbottom: 0,
-      width: '100%',
-      height: 80,
-      alignItems: 'center',
-    },
-    textoRodape:{
-      color: '#ffffff',
-      textAlign: 'center',
-      marginbottom: 7,
-    },
-
-    linkRodape:{
-      color: '#e6b061',
-      fontWeight: 'bold',
-      textDecorationLine: 'none',
-      textAlign: 'center',
-    },
-
+          { /*=========== RODAPÉ =============*/}
+          { /* Parte final da página */}
+          <View style={styles.rodape}>
+            { /* Texto de direitos de autorais */}
+            <Text style={styles.textoRodape}> 2026 TechEduca. Todos os direitos reservados.</Text>
+  
+            { /* Links de Contato */}
+            <Link href='/contato'>
+              <Text style={styles.linkRodape}>Entre em contato</Text>'
+            </Link>
+          </View>
+  
+      </ScrollView>
+   );
   }
-);
+  
+  const styles = StyleSheet.create(
+    {
+      topo: {
+        backgroundColor: '#1a4db3',
+        padding:20,
+        alignItems: 'center',
+        gap: 10,
+      },
+  
+      logoP1: {
+        color:'#ffffff',
+        fontSize:24,
+        fontWeight: 'bold',
+      },
+  
+      logoP2: {
+        color:'#ff6a00',
+        fontSize:24,
+        fontWeight: 'bold',
+      },
+  
+      menu: {
+        marginTop: 10,
+        alignItems: 'center',
+        gap: 10,
+      },
+  
+      menuItem: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+      },
+  
+      ativo: {
+        color: '#ff6a00',
+      },
+  
+     
+  
+      rodape: {
+        backgroundColor: '#1a4db3',
+        padding: 20,
+        alignItems: 'center',
+        gap: 8,
+      },
+  
+      textoRodape: {
+        color : '#ffffff',
+        textAlign: 'center', 
+        marginBottom: 8,
+      },
+  
+      linkRodape: {
+        color: '#ff6a00',
+        fontWeight: 'bold',
+        textDecorationLine: 'none'
+      },
+  
+      tituloDestaque : {
+        color: '#1a4db3',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20
+      }
+  
+    }
+  )
--- cafe-central/app/(tabs)/sobre.js
+++ cafe-central/app/(tabs)/sobre.js
@@ -30,6 +30,32 @@ export default function sobre() {
 
 
       { /* Conteudo da pagina*/}
+      <View style={styles.sobre}>
+
+          <Text style={styles.titulo}>Bem-Vindo ao Café Central</Text>
+
+          <View style={styles.cards}>
+
+        <View style={styles.card}>
+        <Text style= {styles.subtitulo}>Sobre o Café Central</Text>
+        <Text style={styles.texto}>O Café Central está localizado em uma região de grande circulação de pessoas, oferecendo praticidade e qualidade no dia a dia.</Text>
+        </View>
+
+        <View style={styles.card}>
+        <Text style={styles.subtitulo}>Nosso Ambiente</Text>
+        <Text style={styles.texto}>Nosso espaço é acolhedor e pensado para todos. Um lugar ideal para estudar, conversar ou simplesmente relaxar. .</Text>
+        </View>
+  
+        <View style={styles.card}>
+        <Text style={styles.subtitulo}>Sobre nosso Serviço</Text>
+        <Text style={styles.texto}>Aqui, cada detalhe é pensado para tornar sua rotina mais leve com café rápido, funcionarios educados, ambiente confortavel e bem limpo!</Text>
+        </View>
+
+
+      </View>
+      </View>
+
+
 
       { /*=========== rodape =============*/}
 
@@ -78,6 +104,48 @@ const styles = StyleSheet.create(
     menuItem:{
       color: '#ffffff',
       fontSize: 16,
+    },
+
+    titulo:{
+      color: '#976739',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      textAlign: 'center',
+    },
+
+    subtitulo:{
+      color: '#976739',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      textAlign: 'center',
+    },
+
+    cards:{
+      marginTop: 20,
+      gap: 15,
+    },
+
+    card:{
+      backgroundColor: '#f5f5f5',
+      padding: 15,
+      borderRadius: 8,
+      marginBottom: 10,
+      elevation: 3,
+      shadowColor: '#000',
+      shadowOpacity: 0.1,
+      shadowRadius: 5,
+      shadowOffset: { width: 0, height: 3 },
+    },
+
+    
+
+    texto:{
+      color: '#000000',
+      fontSize: 16,
+      marginBottom: 20,
+      textAlign: 'center',
     },
     
 
